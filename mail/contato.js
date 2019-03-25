34aaa4113127f9506f41320219daddbdcdcb5b53
Welici Jacinto de Araujo <welici.araujo@ofm.local>
2019-03-25 17:12:00 -0300

    Melhor versão do site

---
+++ mail/contato.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('btn-enviar-form');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    let inputName = document.querySelector()
+});
+
+function verificaCampos() {
+
+} 
