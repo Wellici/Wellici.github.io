8b7043f3114396c44b797bc3e671734a9fb8559c
Welici Jacinto de Araujo <welici.araujo@ofm.local>
2019-03-26 16:08:08 -0300

    Retirei algumas imagens desnecessarias, mudei a cor do navbar quando está no modo mobile e fiz algumas alterações no html

--- mail/contato.js
+++ mail/contato.js
@@ -1,10 +1,42 @@
-let button = document.querySelector('btn-enviar-form');
+// Capturei o botao que abre o formulario
+let btn_contato = document.querySelector('#entrar-contato');
 
-button.addEventListener('click', function(event) {
-    event.preventDefault();
-    let inputName = document.querySelector()
+// Mandei o botao que abre meu formulario ficar ouvindo um evento de click
+btn_contato.addEventListener('click', function(event){
+    //event.preventDefault();
+    // capturei a lista de inputs do meu fomulario
+    let inputs = document.querySelectorAll('input.form-control'),
+        enviar = document.querySelector('#btn-enviar-form'),
+        textarea = document.getElementById('textarea');
+    setInterval(() => {
+        if(verificaCampos(inputs)){
+            enviar.removeAttribute('disabled');
+            enviar.addEventListener('click', () => {
+
+                //limpaForms(inputs);
+                //textarea.value = '';
+            });
+        }   
+    }, 1000);
 });
 
-function verificaCampos() {
-
+// Se todos os campos estiverem preechidos essa função vai retornar true
+function verificaCampos(listInput) {
+    let todosPreechidos = false,
+        cont = 0;
+    listInput.forEach(element => {
+        if(element.value != ''){
+           cont ++;
+        }
+    });
+    if(cont == 3){
+        todosPreechidos = true;
+    }
+    return todosPreechidos;
 } 
+
+function limpaForms(listInput){
+    listInput.forEach(element => {
+        element.value = '';
+    });
+}
